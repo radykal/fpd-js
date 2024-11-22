799392f8ce9ec81cc62e5e1c1a7e3a8f3612af6d
Rafael Dery <webmaster@radykal.de>
2024-11-22 10:04:59 +0100

    blendmode

--- src/fabricjs/Element.js
+++ src/fabricjs/Element.js
@@ -420,7 +420,9 @@ fabric.Object.prototype.changeColor = function (colorData, colorLinking = true) 
 		//colorize png or dataurl image
 		if ((colorizable == "png" || colorizable == "dataurl") && colorData) {
 			this.filters = [];
-			this.filters.push(new fabric.Image.filters.BlendColor({ mode: this.colorMode, color: colorData }));
+			this.filters.push(
+				new fabric.Image.filters.BlendColor({ mode: this.colorMode, color: colorData, alpha: 1 })
+			);
 
 			this.applyFilters();
 			this.canvas.renderAll();
